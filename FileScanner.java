884c2c2bd172e4112f62f039a4d133b6c90952f7
sabba786 <sabba02.se@gmail.com>
2025-12-03 22:42:57 +0100

    file scanner added

--- FileScanner.java
+++ FileScanner.java
@@ -1,5 +1,40 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class FileScanner {
     public static void main(String[] args) {
-        
+        String command = args[0];
+        String wordToFind = args[1];
+        String filePath = args[2];
+
+        switch (command) {
+            case "search":
+                try {
+                    File file = new File(filePath);
+
+                    Scanner scanner = new Scanner(file);
+
+                    while (scanner.hasNextLine()) {
+                        String line = scanner.nextLine();
+                        if (line.contains(wordToFind)) {
+                            System.out.println(line);
+                        }
+                    }
+
+                    scanner.close();
+
+                } catch (FileNotFoundException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+
+                break;
+
+            default:
+                System.out.println("Command " + command + " not recognised.");
+                break;
+        }
+
     }
 }
